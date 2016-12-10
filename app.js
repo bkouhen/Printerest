4b52bce280ed654316ad94f0acbc49fc4f0ef7e3
bkouhen <b.kouhen@gmail.com>
2016-12-10 15:44:26 +0100

    Adding scrolling animations

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ var main = function() {
 			$(this).attr('src', 'images/images_site/Logo_mini_off.png');
 		});
 		
-
-	
+	//Expansion du bouton 'S'inscrire maintenant'
 	$('.expand_btn_success').hover(
 	
 	function(){
@@ -37,6 +36,45 @@ var main = function() {
 	
 	);
 	
+	//Changement du curseur 'active' pour la Navbar
+	$('.navbar-nav a').click(function(){
+		$('.active').removeClass('active');
+		$(this).parent().addClass('active');
+	});
+	
+	$('.btn_scroll').click(function(){
+		var scr = $(document).scrollTop();
+		alert("You're at " + scr + " pixels from the top");
+	});
+	
+	//Scrolling features (Show containers)
+	$('.content_cont').hide();
+	$('.signup_cont').hide();
+	
+	$(document).scroll(function(){
+		var y = $(this).scrollTop();
+		if (y > 82) {$('.content_cont').fadeIn(3000);}
+		if (y > 222) {
+			$('body nav').css('opacity','0.5');
+		} else {
+			$('body nav').css('opacity','1');
+		}
+		if (y > 654) {$('.signup_cont').fadeIn(3000);}
+	});
+	
+	
+/*
+	$('.content_cont').hide();
+	$('.signup_cont').hide();
+	
+	$('#header').mouseleave(function(){
+		$('.content_cont').fadeIn(3000);
+	});
+	
+	$('.content_cont').mouseleave(function(){
+		$('.signup_cont').fadeIn(3000);
+	});
+*/
 	
 /*
 	$('body').hide();
